4b892d6368a6be618146088bba623633d0da8e7a
Matti Hiltunen <mtti@users.noreply.github.com>
2020-04-19 21:23:26 +0300

    Fix EditorGUIUtils

--- Editor/EditorGUIUtils.cs
+++ Editor/EditorGUIUtils.cs
@@ -14,7 +14,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-namespace mtti.Funcs
+using UnityEngine;
+using UnityEditor;
+
+namespace mtti.Funcs.Editor
 {
     public static class EditorGUIUtils
     {
