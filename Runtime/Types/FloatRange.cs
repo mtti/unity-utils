cba39bd5742c706f702693e84930ad6fd12026ac
Matti Hiltunen <mtti@users.noreply.github.com>
2023-01-06 01:04:19 +0200

    FloatRange: Add .Lerp()

--- Runtime/Types/FloatRange.cs
+++ Runtime/Types/FloatRange.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace mtti.Funcs
 {
@@ -12,7 +13,10 @@ namespace mtti.Funcs
         [SerializeField]
         public float Max;
 
-        public FloatRange(float min, float max)
+        public FloatRange(
+            float min,
+            float max
+        )
         {
             if (min > max)
             {
@@ -25,12 +29,17 @@ namespace mtti.Funcs
             Max = max;
         }
 
+        public float Lerp(float t)
+        {
+            return Mathf.Lerp(Min, Max, t);
+        }
+
         /// <summary>
         /// Get a random value in the range (inclusive).
         /// </summary>
         public float GetRandom()
         {
-            return UnityEngine.Random.Range(Min, Max);
+            return Random.Range(Min, Max);
         }
     }
 }
